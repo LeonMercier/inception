b54a78dc5e90444c9ecf5ce9b249a4390a718674
lemercie <lemercie@student.hive.fi>
2025-08-28 12:16:51 +0300

    create databse user, add .env example

--- srcs/requirements/mariadb/setup.sh
+++ srcs/requirements/mariadb/setup.sh
@@ -1,3 +1,24 @@
 #!/bin/sh
 
-mariadb-install-db --basedir=/usr --datadir=/var/lib/mariadb --user=mariadbuser
+# mariadb-install-db creates a folder called 'mysql' in the datadir
+# "if directory doesn't exist"
+if [ ! -d "/var/lib/mariadb/mysql" ]; then
+	mariadb-install-db --basedir=/usr --datadir=/var/lib/mariadb \
+		--user=mariadbuser
+
+	# $DB_USER@'%' => allow user to connect from any host; % is wildcard
+	# $DB_NAME.* => all tables in that database
+	# hyphen after << means heredoc will strip leading whitespace => we can 
+	# indent our heredoc nicely
+	mariadbd --user=mariadbuser <<- EOF
+		USE mysql;
+		ALTER USER 'root'@'localhost' IDENTIFIED BY "$DB_ROOT_PW";
+		CREATE DATABASE $DB_NAME;
+		CREATE USER $DB_USER@'%' IDENTIFIED BY "$DB_USER_PW";
+		GRANT ALL PRIVILEGES ON $DB_NAME.* TO $DB_USER@'%';
+	EOF
+fi
+
+# exec replaces current shell with the command, docker needs the process to
+# be PID 1, so that signals are correctly propagated
+exec mariadbd-safe
